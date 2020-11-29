9b4666b5d8ce23027e4111b0fd82f9b56fa6d969
anitacaron <annycaron@gmail.com>
2020-11-29 22:34:19 +0000

    implemented both exercises

--- src/main/java/com/techreturners/exercise001/Exercise001.java
+++ src/main/java/com/techreturners/exercise001/Exercise001.java
@@ -1,30 +1,39 @@
 package com.techreturners.exercise001;
 
-import java.util.List;
+import java.util.List; 
 
 public class Exercise001 {
     public String capitalizeWord(String word) {
-        // Add your code here
-        return "";
+        return Character.toUpperCase(word.charAt(0)) + word.substring(1);
     }
 
     public String generateInitials(String firstName, String lastName) {
-        // Add your code here
-        return "";
+        return firstName.charAt(0) + "." + lastName.charAt(0);
     }
 
     public double addVat(double originalPrice, double vatRate) {
-        // Add your code here
-        return 0.0;
+        double scale = Math.pow(10, 2);
+        double value = originalPrice * (1 + (vatRate / 100));
+        
+        return Math.round(value * scale) / scale;
     }
 
     public String reverse(String sentence) {
-        // Add your code here
-        return "";
+        String rev_sentence= "";
+        for(int i = sentence.length()-1; i>=0; i--) {
+            rev_sentence = rev_sentence + sentence.charAt(i);
+        }
+        return rev_sentence;
     }
 
     public int countLinuxUsers(List<User> users) {
-        // Add your code here
-        return 0;
+        int count_linux_users = 0;
+        for (User user : users) {
+            if (user.getType() == "Linux") {
+                count_linux_users++;
+            }
+        }
+
+        return count_linux_users;
     }
 }
